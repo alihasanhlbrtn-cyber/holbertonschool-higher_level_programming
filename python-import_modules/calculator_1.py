df1d170182bb5694967423a5f616024a73c5055d
alihasanhlbrtn-cyber <alihasanhlbrtn@gmail.com>
2025-11-17 16:57:26 +0000

    simple calculator

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys 
+def add(a, b):
+    result = a + b
+    return result
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return int(a / b)
