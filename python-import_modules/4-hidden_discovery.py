02d89499c5a680063c120bf9f4dc77eafc6f8243
alihasanhlbrtn-cyber <alihasanhlbrtn@gmail.com>
2025-11-18 11:01:43 +0000

    who am i?

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# accepting inputs dynamically
+if __name__ == "__main__":
+
+
+    import hidden_4
+    for i in sorted(dir(hidden_4)):
+        if not i.startswith("__"):
+            print(i)
