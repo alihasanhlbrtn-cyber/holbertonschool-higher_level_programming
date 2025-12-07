081f63fc7ce48fa10649941d7ba847129662e574
alihasanhlbrtn-cyber <alihasanhlbrtn@gmail.com>
2025-12-07 16:28:15 +0400

    POST an email

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a given URL with an email parameter
+and displays the body of the response.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+    print(response.text)
