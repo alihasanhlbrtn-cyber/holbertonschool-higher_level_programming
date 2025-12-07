1e5193141726dbf2e885487fb961fdc7de3939c4
alihasanhlbrtn-cyber <alihasanhlbrtn@gmail.com>
2025-12-07 16:08:01 +0400

    upgrade

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Takes in a URL, sends a request to the URL and displays the value
+of the X-Request-Id variable found in the response header.
+"""
 import sys
 import urllib.request
 
