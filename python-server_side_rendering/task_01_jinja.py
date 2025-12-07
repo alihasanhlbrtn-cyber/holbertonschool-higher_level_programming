70e52f9526465f3867af105b7e5b956fb764fdf5
alihasanhlbrtn-cyber <alihasanhlbrtn@gmail.com>
2025-12-07 17:13:36 +0400

    Creating a Basic HTML Template in Flask

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Basic Flask app that renders Jinja templates with shared
+header and footer on multiple pages.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
