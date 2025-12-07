c799fd5b9f4c5e14d66ad5b488791a847edbc34b
alihasanhlbrtn-cyber <alihasanhlbrtn@gmail.com>
2025-12-07 16:35:25 +0400

    Error code #1

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and displays the body of the response.
+
+- If HTTP status code >= 400, prints: "Error code: <status_code>"
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
